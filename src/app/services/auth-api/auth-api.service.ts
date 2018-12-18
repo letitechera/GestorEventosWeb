10bf41a7d810eaadaab4ff4f049783f7ffb2013d
Leticia Techera <leticia.tech@gmail.com>
2018-12-18 00:25:55 -0300

    Serv

--- src/app/services/auth-api/auth-api.service.ts
+++ src/app/services/auth-api/auth-api.service.ts
@@ -5,7 +5,6 @@ import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthApiService {
-  private headers: HttpHeaders;
 
   constructor(private http: HttpClient, private router: Router) {
   }
@@ -18,11 +17,10 @@ export class AuthApiService {
     return this.commonHttpPost(`${environment.webApiUrl}/auth/login`, data, null);
   }
 
-  public setSession(data){
-    console.log(data);
-    // const expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
-    // localStorage.setItem('access_token', authResult.accessToken);
-    // localStorage.setItem('expires_at', expiresAt);
+  public setSession(data) {
+    const expiresAt = JSON.stringify((data.expires_in * 1000) + new Date().getTime());
+    localStorage.setItem('eg_auth_token', data.auth_token);
+    localStorage.setItem('expires_in', expiresAt);
   }
 
   public isLogged() {
@@ -31,18 +29,25 @@ export class AuthApiService {
   }
 
   private getAccessToken() {
-    return localStorage.getItem('access_token');
+    return localStorage.getItem('eg_auth_token');
   }
 
   private deleteSession() {
-    localStorage.removeItem('access_token');
-    // localStorage.removeItem('expires_at');
+    localStorage.removeItem('eg_auth_token');
+    localStorage.removeItem('expires_in');
     localStorage.clear();
   }
 
   public logout() {
     this.deleteSession();
     this.router.navigateByUrl('login');
+  }
+
+  public isAuthenticated(): boolean {
+    // Check whether the current time is past the
+    // access token's expiry time
+    const expiresAt = JSON.parse(localStorage.getItem('expires_in'));
+    return new Date().getTime() < expiresAt;
   }
 
   private setDefaultHeaders() {
@@ -63,4 +68,5 @@ export class AuthApiService {
   private commonHttpPatch(url: string, data: any, headers: HttpHeaders) {
     return this.http.patch(url, data, { headers: headers });
   }
+
 }
