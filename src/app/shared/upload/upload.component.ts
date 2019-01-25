06dda8f5209c037377a7303503a827b43867b9a3
Leticia Techera <leticia.tech@gmail.com>
2019-01-24 23:51:55 -0300

    Frontend File Upload

--- src/app/shared/upload/upload.component.ts
+++ src/app/shared/upload/upload.component.ts
@@ -15,27 +15,42 @@ export class UploadComponent implements OnInit {
 
   constructor(private http: HttpClient) { }
  
+  public formData: FormData;
+  public fileLoaded: boolean;
+
   ngOnInit() {
+    this.formData = new FormData();
   }
  
   public uploadFile = (files) => {
     if (files.length === 0) {
       return;
     }
- 
     let fileToUpload = <File>files[0];
-    const formData = new FormData();
-    formData.append('file', fileToUpload, fileToUpload.name);
- 
-    this.http.post(`${this.baseurl}`, formData, {reportProgress: true, observe: 'events'})
-      .subscribe(event => {
-        if (event.type === HttpEventType.UploadProgress)
-          this.progress = Math.round(100 * event.loaded / event.total);
-        else if (event.type === HttpEventType.Response) {
-          this.message = 'Upload success.';
-          this.onUploadFinished.emit(event.body);
-        }
-      });
+    this.formData.append('file', fileToUpload, fileToUpload.name);
+    this.fileLoaded = true;
+    debugger;
+  }
+
+  public saveFile() {
+    if (!this.fileLoaded) {
+      return;
+    }
+    this.http.post(`${this.baseurl}/${1}`, this.formData, {reportProgress: true, observe: 'events'})
+    .subscribe(event => {
+      if (event.type === HttpEventType.UploadProgress)
+        this.progress = Math.round(100 * event.loaded / event.total);
+      else if (event.type === HttpEventType.Response) {
+        this.message = 'Upload success.';
+        this.fileLoaded = false;
+        this.onUploadFinished.emit(event.body);
+      }
+    });
+  }
+
+  public clearFiles(){
+    this.formData = new FormData();
+    this.fileLoaded = false;
   }
 
 }
